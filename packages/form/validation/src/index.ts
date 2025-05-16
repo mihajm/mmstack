91d08df4b2ce3c971df64f1b03980b5c5d7ccff6
Miha Mulec <miham@marand.si>
2025-05-16 09:42:13 +0200

    fix: export file validator options

--- packages/form/validation/src/index.ts
+++ packages/form/validation/src/index.ts
@@ -1,5 +1,6 @@
 export type { ArrayValidatorOptions } from './lib/array';
 export * from './lib/date/public_api';
+export type { FileValidatorOptions } from './lib/file';
 export type { NumberValidatorOptions } from './lib/number';
 export type { StringValidatorOptions } from './lib/string';
 export * from './lib/validators';

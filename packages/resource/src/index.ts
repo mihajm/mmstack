1e5999531ea2a4adfa70072cf11409d46440efce
Miha J. Mulec <mihavenxly@gmail.com>
2026-02-17 14:06:19 +0100

    infra: remove intermediary public_api.ts for resource lib

--- packages/resource/src/index.ts
+++ packages/resource/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/public_api';
+export * from './lib/manual-query';
+export * from './lib/mutation-resource';
+export * from './lib/query-resource';
+export * from './lib/util/public_api';
--- packages/resource/src/lib/public_api.ts
+++
@@ -1,4 +0,0 @@
-export * from './manual-query';
-export * from './mutation-resource';
-export * from './query-resource';
-export * from './util/public_api';

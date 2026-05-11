643f71ff39646199af086ced76f52fa49263abee
Miha J. Mulec <mihavenxly@gmail.com>
2026-05-11 12:06:25 +0200

    infra(translate): remerge types

--- packages/translate/src/lib/parameterize.type.ts
+++ packages/translate/src/lib/parameterize.type.ts
@@ -1,4 +1,5 @@
-import { UnknownStringKeyObject } from './string-key-object.type';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { type UnknownStringKeyObject } from './string-key-object.type';
 
 type Simplify<T> = T extends infer U
   ? { [K in keyof U]: Simplify<U[K]> }
@@ -32,11 +33,23 @@ type extractComplexParam<T extends string> = T extends
     ? extractSelectParam<VarName, `${SelectOptions}}`> | extractParams<REST>
     : never;
 
+type IsSimpleIdent<T extends string> = T extends ''
+  ? false
+  : T extends
+        | `${string} ${string}`
+        | `${string},${string}`
+        | `${string}{${string}`
+        | `${string}#${string}`
+    ? false
+    : true;
+
 export type extractParams<T extends string> =
   T extends `${infer _Start}{${infer Var}}${infer End}`
     ? Var extends `${infer _}, ${infer __}`
       ? extractComplexParam<`{${Var}}${End}`>
-      : [Var, string] | extractParams<End>
+      : IsSimpleIdent<Var> extends true
+        ? [Var, string] | extractParams<End>
+        : extractParams<End>
     : never;
 
 type mergeParams<TExtracted extends [string, any]> = {
